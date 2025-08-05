a5556b7e98dd7f3fd268dbe21352f94397a81ae2
Pavel Grancharov <dvdtygc@gmail.com>
2025-08-05 11:59:22 +0300

    Add load balancer.

--- main.tf
+++ main.tf
@@ -7,16 +7,23 @@ variable "eu-central-1-ubuntu-ami" {
   default = "ami-0a87a69d69fa289be"
 }
 
-resource "aws_launch_configuration" "example" {
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_launch_template" "example" {
   image_id      = var.eu-central-1-ubuntu-ami
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
-  user_data = <<-EOF
-    #!/bin/bash
-    echo "Hello, World" > index.html
-    nohup busybox httpd -f -p ${var.server_port} &
-  EOF
+  user_data = base64encode(<<-EOF
+      #!/bin/bash
+      echo "Hello, World" > index.html
+      nohup busybox httpd -f -p ${var.server_port} &
+    EOF
+  )
 
   lifecycle {
     create_before_destroy = true
@@ -24,8 +31,15 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "asg_example" {
-  launch_configuration = aws_launch_configuration.example.name
-  vpc_zone_identifier = data.aws_subnet.default.id
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  health_check_type   = "ELB"
+
+  target_group_arns = [aws_lb_target_group.auto_scaling_group.arn]
 
   min_size = 2
   max_size = 3
@@ -37,11 +51,67 @@ resource "aws_autoscaling_group" "asg_example" {
   }
 }
 
+resource "aws_lb" "example" {
+  name               = "example-lb"
+  subnets            = data.aws_subnets.default.ids
+  load_balancer_type = "application"
+  security_groups = [aws_security_group.application_load_balancer.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  # By default, return a simple 404 page
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.auto_scaling_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "auto_scaling_group" {
+  name     = "terraform-asg-example"
+  port     = var.server_port
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
 data "aws_vpc" "default" {
   default = true
 }
 
-data "aws_subnet" "default" {
+data "aws_subnets" "default" {
   filter {
     name = "vpc-id"
     values = [data.aws_vpc.default.id]
@@ -59,13 +129,25 @@ resource "aws_security_group" "instance" {
   }
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
+resource "aws_security_group" "application_load_balancer" {
+  name = "terraform-example-alb"
+  # Allow inbound HTTP requests
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Allow all outbound requests
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-output "server_ip" {
-  value       = aws_instance.example.public_ip
-  description = "The public IP address of the example instance"
+output "alb_dns_name" {
+  value       = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }
