68669476570ececce8da25132c7a342e4d1cf1e5
Pavel Grancharov <dvdtygc@gmail.com>
2025-08-06 13:50:19 +0300

    Update examples.

--- workspaces/main.tf
+++ workspaces/main.tf
@@ -1,10 +1,19 @@
 terraform {
   backend "s3" {
-    region       = "eu-central-1"
-    key          = "workspaces-example/terraform.tfstate"
-    bucket       = "dvdty-example-terraform-state2"
-    encrypt      = true
-    use_lockfile = true
+    region  = "eu-central-1"
+    key     = "workspaces-example/terraform.tfstate"
+    bucket  = "dvdty-example-terraform-state3"
+    encrypt = true
+
+    use_lock_table = true
+    lock_table     = "terraform-up-and-running-locks3"
+  }
+
+  required_providers {
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = "1.23.0"
+    }
   }
 }
 
@@ -13,7 +22,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "dvdty-example-terraform-state2"
+  bucket = "dvdty-example-terraform-state3"
 
   lifecycle {
     prevent_destroy = true
@@ -47,7 +56,28 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-up-and-running-locks3"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0a87a69d69fa289be"
+  instance_type = "t2.nano"
+}
+
 output "s3_bucket_arn" {
   value       = aws_s3_bucket.terraform_state.arn
   description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value       = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
