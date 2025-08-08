6367f53720d83291bd7f0d5cfce631bcb671603b
Pavel Grancharov <dvdtygc@gmail.com>
2025-08-08 14:33:06 +0300

    Use resources instead of inline config blocks.

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -103,18 +103,32 @@ resource "aws_security_group" "webserver" {
 
 resource "aws_security_group" "application_load_balancer" {
   name = "${var.webserver_name}-security-group-application-load-balancer"
+}
 
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+locals {
+  http_port    = 80
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+  any_port     = 0
+  any_protocol = "-1"
+}
 
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.application_load_balancer.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.application_load_balancer.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
 }
